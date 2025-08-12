b14ba9b4c51d0cbe9909e3565377feb2c9181a12
renz93 <renzgha@gmail.com>
2025-08-12 14:54:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+//Import the THREE.js library
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+// To allow for the camera to move around the scene
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// To allow for importing the .gltf file
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
 // --- Smooth nav button scrolling ---
 document.querySelectorAll('.nav-buttons .btn').forEach(btn => {
   btn.addEventListener('click', () => {
